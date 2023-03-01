a91f94e724769b1d29a0f7abcc3b63390b8a919b
focusdroid <840254112@qq.com>
2023-03-01 16:53:56 +0800

    featrue(update):更新偶尔登录失效问题question

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -4,11 +4,7 @@ import {message} from "antd";
 
 
 class Request {
-    private headers: { token: string };
     constructor() {
-        this.headers = {
-            token: localStorage.token || ''
-        }
         this.refleshToken()
     }
     HttpRequest (url: string, methods: string = 'GET', params: any = {}) { // 将所有请求整合移植
@@ -21,7 +17,10 @@ class Request {
         }
         const options = Object.assign(fetchOption, {
             method: methods.toUpperCase(),
-            headers: this.headers}
+            headers: {
+                token: localStorage.getItem("token") || ''
+                }
+            }
         )
         return fetch(url,options).then((res: any) => {
             console.log("res--->", res)
@@ -63,30 +62,14 @@ class Request {
         }
          return fetch(url,{
             method: "GET",
-            headers: this.headers
+            headers: {
+                token: localStorage.getItem("token") || ''
+            }
         }).then((res: any) => {
              const { status, ok } = res
              if (status === 200 && ok) {
                  return res.json()
              }
-        })
-    }
-    PostRequest (url: string, params: Object = {}) {
-        return fetch(url, {
-            method: 'POST',
-            headers: this.headers,
-            body: JSON.stringify(params)
-        }).then((res:any) => {
-            const { status, ok } = res
-            /*if (res.data.data.code === '2001') {
-                message.error("登录失效，请重新登陆")
-                window.location.href = "/login"
-            }*/
-            if (status === 200 && ok) {
-                return res.json()
-            } else {
-                return {}
-            }
         })
     }
     refleshToken () {
@@ -100,7 +83,7 @@ class Request {
             return
         }
         timer = setInterval(() =>{
-            RefreshToken({token: this.headers.token}).then(res => {
+            RefreshToken({token: localStorage.getItem("token")}).then(res => {
                 if (res.code === "200") {
                     localStorage.token = ""
                     localStorage.token = res.token
