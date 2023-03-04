58d2462bad229fc0aaa48e85ca205de16d9a948b
focusdroid <840254112@qq.com>
2023-03-04 22:00:20 +0800

    featrue(update):更新样式字段，调整详情显示方式

--- src/view/resumeManagement/TableList.tsx
+++ src/view/resumeManagement/TableList.tsx
@@ -11,10 +11,71 @@ interface IProps {
     freshSource?: (values: searchField) => searchField | null | undefined | void
 }
 
+const getLevelField = (level: string | undefined, jobbed: string | undefined) => {
+    if (level && jobbed) {
+        return `${level} ${jobbed}`
+        // return `${levelField.get(level as string)} ${jobbed}`
+    }
+    if (jobbed) {
+        return <span>{jobbed}</span>
+    }
+    if (level) {
+        return <span>{level}</span>
+    }
+}
+
+interface IPropsDrawTile {
+    resumeDetail: any,
+    open: boolean,
+    setOpen: () => void
+}
+
+const DrawTile = (props:IPropsDrawTile, _:any) => {
+    const {resumeDetail, open } = props
+    return <Drawer
+        title={`${resumeDetail?.name} 简历详情  / 当前年月日: ${new Date().toLocaleDateString().replaceAll("/", "-")}`}
+        placement="right"
+        width={800}
+        onClose={() => props.setOpen()}
+        keyboard
+        open={open}>
+        <Card>
+            <Row gutter={[16, 16]}>
+                <Col span={8}><span className={styles.fontStyle}>姓名: {resumeDetail?.name}</span></Col>
+                <Col span={8}><span className={styles.fontStyle}>性别: {resumeDetail?.gender}</span></Col>
+                <Col span={8}><span
+                    className={styles.fontStyle}>级别: {getLevelField(resumeDetail?.level, resumeDetail?.jobbed)}</span></Col>
+                <Col span={8}><span className={styles.fontStyle}>邮箱: {resumeDetail?.email}</span></Col>
+                <Col span={8}><span className={styles.fontStyle}>电话: {resumeDetail?.phone}</span></Col>
+                <Col span={8}><span className={styles.fontStyle}>工作年限: {resumeDetail?.jobbed_year}年</span></Col>
+                <Col span={8}><span className={styles.fontStyle}>是否重点关注: {resumeDetail?.follow ?
+                    <Tag style={{marginLeft: 10}} color="#87d068">
+                        重点关注
+                    </Tag> : ""}</span></Col>
+            </Row>
+        </Card>
+        <Card style={{marginTop: 10}}>
+            <Row gutter={[16, 16]}>
+                <Col span={8}><span className={styles.fontStyle}>目标公司: {resumeDetail?.target_company}</span></Col>
+                <Col span={8}><span className={styles.fontStyle}>入职负责人: {resumeDetail?.person_charge}</span></Col>
+                <Col span={8}><span className={styles.fontStyle}>几号入职: {resumeDetail?.time_induction}</span></Col>
+                <Col span={8}><span className={styles.fontStyle}>岗位工资: {resumeDetail?.post_salary}</span></Col>
+                <Col span={8}><span className={styles.fontStyle}>入职意向: {resumeDetail?.employment_intention}</span></Col>
+                <Col span={8}><span className={styles.fontStyle}>首次联系时间: {resumeDetail?.first_contact_time}</span></Col>
+                <Col span={8}><span className={styles.fontStyle}>备注信息: {resumeDetail?.remarks}</span></Col>
+            </Row>
+        </Card>
+        <Button style={{marginTop: 10}} type="primary">在线查看简历</Button>
+    </Drawer>
+}
+
 const TableList:FC<IProps> = (props) =>{
     const [resumeDetail, setResumeDetail] = useState<ResumeObj>()
     const navigator = useNavigate()
     const [open, setOpen] = useState<boolean>(false);
+    function changeOpenStatus () {
+        setOpen(false)
+    }
     const columns = [
         {
             title: '姓名',
@@ -25,41 +86,7 @@ const TableList:FC<IProps> = (props) =>{
             render: (text:any, record:ResumeObj, _:any) => <div>
                 <span style={{color: "blue", cursor: "pointer"}} onClick={() => getCurrentDetail(record)}>{text}</span>
                 {record.follow ? <Tag style={{marginLeft: 10}} color="#87d068">重点关注 </Tag> : ""}
-                <Drawer
-                    title={`${resumeDetail?.name} 简历详情  / 当前年月日: ${new Date().toLocaleDateString().replaceAll("/", "-")}`}
-                    placement="right"
-                    width={800}
-                    onClose={() => setOpen(false)}
-                    keyboard
-                    open={open}>
-                    <Card>
-                        <Row gutter={[16, 16]}>
-                            <Col span={8}><span className={styles.fontStyle}>姓名: {resumeDetail?.name}</span></Col>
-                            <Col span={8}><span className={styles.fontStyle}>性别: {resumeDetail?.gender}</span></Col>
-                            <Col span={8}><span
-                                className={styles.fontStyle}>级别: {getLevelField(resumeDetail?.level, resumeDetail?.jobbed)}</span></Col>
-                            <Col span={8}><span className={styles.fontStyle}>邮箱: {resumeDetail?.email}</span></Col>
-                            <Col span={8}><span className={styles.fontStyle}>电话: {resumeDetail?.phone}</span></Col>
-                            <Col span={8}><span className={styles.fontStyle}>工作年限: {resumeDetail?.jobbed_year}年</span></Col>
-                            <Col span={8}><span className={styles.fontStyle}>是否重点关注: {resumeDetail?.follow ?
-                                <Tag style={{marginLeft: 10}} color="#87d068">
-                                    重点关注
-                                </Tag> : ""}</span></Col>
-                        </Row>
-                    </Card>
-                    <Card style={{marginTop: 10}}>
-                        <Row gutter={[16, 16]}>
-                            <Col span={8}><span className={styles.fontStyle}>目标公司: {resumeDetail?.target_company}</span></Col>
-                            <Col span={8}><span className={styles.fontStyle}>入职负责人: {resumeDetail?.person_charge}</span></Col>
-                            <Col span={8}><span className={styles.fontStyle}>几号入职: {resumeDetail?.time_induction}</span></Col>
-                            <Col span={8}><span className={styles.fontStyle}>岗位工资: {resumeDetail?.post_salary}</span></Col>
-                            <Col span={8}><span className={styles.fontStyle}>入职意向: {resumeDetail?.employment_intention}</span></Col>
-                            <Col span={8}><span className={styles.fontStyle}>首次联系时间: {resumeDetail?.first_contact_time}</span></Col>
-                            <Col span={8}><span className={styles.fontStyle}>备注信息: {resumeDetail?.remarks}</span></Col>
-                        </Row>
-                    </Card>
-                    <Button style={{marginTop: 10}} type="primary">在线查看简历</Button>
-                </Drawer>
+                <DrawTile resumeDetail={resumeDetail} open={open} setOpen={changeOpenStatus}/>
             </div>
         },
         {
@@ -77,12 +104,13 @@ const TableList:FC<IProps> = (props) =>{
             title: '邮箱',
             dataIndex: 'email',
             key: 'email',
+            width: 200,
         },
         {
             title: '性别',
             dataIndex: 'gender',
             key: 'gender',
-            width: 100,
+            width: 70,
             render: (text:string, record:ResumeObj,_:any) => <span>
                 {text === 'M' ? '男' : '女'}
             </span>
@@ -100,6 +128,7 @@ const TableList:FC<IProps> = (props) =>{
             title: '首次联系时间',
             dataIndex: 'first_contact_time',
             key: 'first_contact_time',
+            width: 200,
             render: (_: any, record: ResumeObj) => {
                 return <span>{record?.first_contact_time ? dayjs(record?.first_contact_time).format("YYYY-MM-DD HH:ss:mm") : null}</span>
             }
@@ -108,6 +137,7 @@ const TableList:FC<IProps> = (props) =>{
             title: '入职意向',
             dataIndex: 'employment_intention',
             key: 'employment_intention',
+            width: 70,
         },
         {
             title: '是否确认入职',
@@ -128,6 +158,7 @@ const TableList:FC<IProps> = (props) =>{
             title: '入职时间',
             dataIndex: 'time_induction',
             key: 'time_induction',
+            width: 200,
             render: (_: any, record: ResumeObj) => {
                 return <span>{record.time_induction ? dayjs(record.time_induction).format("YYYY-MM-DD HH:ss:mm") : null}</span>
             }
@@ -156,18 +187,6 @@ const TableList:FC<IProps> = (props) =>{
             </div>
         },
     ];
-    function getLevelField(level: string | undefined, jobbed: string | undefined) {
-        if (level && jobbed) {
-            return `${level} ${jobbed}`
-            // return `${levelField.get(level as string)} ${jobbed}`
-        }
-        if (jobbed) {
-            return <span>{jobbed}</span>
-        }
-        if (level) {
-            return <span>{level}</span>
-        }
-    }
     const {list} = props
     return <Table
         style={{marginTop: 10}}
